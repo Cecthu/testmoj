d543273c14710daf1ffa9440d23e7b859adf0c9f
Cecilia Thulin <cilla@Peach.local>
2018-11-07 13:18:09 +0100

    first try

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+public class IfElse {
+
+  public static void main(String[] args){
+
+    System.out.println("Jämna tal från 2 till 20");
+
+    int number = 2;
+    while(number<=12){
+      System.out.println(number);
+      number += 2;
+    }
+
+    System.out.println("räknar ner 40 till 30");
+
+    for(int i = 40;i>=30;i--){
+      System.out.println(i);
+    }
+  }
+}
